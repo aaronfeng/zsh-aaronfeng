acd75ea6ada140c26104f5961cea064e1bfe0f28
Aaron Feng <aaron@forty9ten.com>
2015-02-04 13:32:16 -0500

    turn it into a fn

--- docker-machine.zsh
+++ docker-machine.zsh
@@ -1,5 +1,8 @@
-CONFIG_NAME="$1"
-shift
-COMMAND="$@"
+docker-machine-wrapper() {
+  CONFIG_NAME="$1"
+  shift
+  COMMAND="$@"
+  docker $(docker-machine config $CONFIG_NAME) $COMMAND
+}
 
-alias dm="docker $(./docker-machine config $CONFIG_NAME) $COMMAND"
+alias dm="docker-machine-wrapper"
